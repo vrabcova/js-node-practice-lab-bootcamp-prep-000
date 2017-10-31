c2163c13ddc086d649fec66bd87d632585326b2d
Stefanie Brabec <sbrabec@me.com>
2017-10-31 01:13:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 var name = "Stef"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1 )
+console.log(height + 1 )
